179009e3e9680e9bcb1044c6d769f3cad7291c1e
Etern1tyDark <nathankhopancras@gmail.com>
2025-09-12 18:56:37 +0700

    fix: dynamic flag for pad (med) cry chall

--- Cryptography/pad/source/pad/entrypoint.sh
+++ Cryptography/pad/source/pad/entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 set -eu
 
-UUID="$(python3 - <<'PY'
-import uuid; print(uuid.uuid4())
-PY
-)"
-FLAG="HCS{how_different_was_this_from_ecb_oracle?_quite_different_huh_${UUID}}"
+FLAG_INIT="HCS{how_different_was_this_from_ecb_oracle?_quite_different_huh_[UUID]}"
+FLAG=${FLAG_INIT//'[UUID]'/$FLAG}
 
 echo -n "$FLAG" > /home/ctf/flag.txt
 chown ctf:ctf /home/ctf/flag.txt
 chmod 400 /home/ctf/flag.txt
 
-unset FLAG UUID
+unset FLAG FLAG_INIT
 
 exec socat TCP-LISTEN:1337,reuseaddr,fork,nodelay,su=ctf \
   "EXEC:'sh -lc \"cd /home/ctf && timeout 60 python3 chall.py\"'"
